f4e190ddc916fb829308b3148e07d2d21738cd4b
alessio rea <alexei.80@hotmail.fr>
2021-04-13 10:03:00 +0200

    getData.py

--- app/getData.py
+++ app/getData.py
@@ -2,4 +2,14 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import tensorflow_datasets as tfds
 
-print('ok')
+
+path = "data/es-en/europarl-v7.es-en.en"
+
+def get_data_text(path) :
+    with open(path, mode = "r", encoding = "utf-8") as f:
+        data_text = f.read()
+    return data_text
+
+
+
+print(get_data_text(path)[:100])
