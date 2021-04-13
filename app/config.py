abd6e9e33bca72de8f14a694e31ac1673a1e2349
alessio rea <alexei.80@hotmail.fr>
2021-04-13 10:19:29 +0200

    progress

---
+++ app/config.py
@@ -0,0 +1,4 @@
+europarl_en_path = "data/es-en/europarl-v7.es-en.en"
+europarl_es_path = "data/es-en/europarl-v7.es-en.es"
+nb_prefix_en_path = "data/nonbreaking_prefix.en"
+nb_prefix_es_path = "data/nonbreaking_prefix.es"
--- app/getData.py
+++ app/getData.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import layers
-import tensorflow_datasets as tfds
+#import tensorflow_datasets as tfds
 
-
-path = "data/es-en/europarl-v7.es-en.en"
 
 def get_data_text(path) :
     with open(path, mode = "r", encoding = "utf-8") as f:
@@ -12,4 +10,15 @@ def get_data_text(path) :
 
 
 
-print(get_data_text(path)[:100])
+if __name__ == "__main__":
+    europarl_en_path = "data/es-en/europarl-v7.es-en.en"
+    europarl_es_path = "data/es-en/europarl-v7.es-en.es"
+    nb_prefix_en_path = "data/nonbreaking_prefix.en"
+    nb_prefix_es_path = "data/nonbreaking_prefix.es"
+
+    europarl_en = get_data_text(europarl_en_path)
+    europarl_es = get_data_text(europarl_es_path)
+    nb_prefix_en = get_data_text(nb_prefix_en_path)
+    nb_prefix_es = get_data_text(nb_prefix_es_path)
+
+    print(nb_prefix_es[:100])
