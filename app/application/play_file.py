71e1f60fcf3dfc22639d53ea5caa5769a88468ca
alessio rea <alexei.80@hotmail.fr>
2021-04-14 08:17:21 +0200

    progress config

--- app/application/play_file.py
+++ app/application/play_file.py
@@ -6,13 +6,6 @@ import tensorflow as tf
 
 tf.keras.backend.clear_session()
 
-D_MODEL = 128 # 512
-NB_LAYERS = 4 # 6
-FFN_UNITS = 512 # 2048
-NB_PROJ = 8 # 8
-DROPOUT_RATE = 0.1 # 0.1
-
-
 
 if __name__ == "__main__":
 
@@ -21,19 +14,19 @@ if __name__ == "__main__":
         input_nb_prefix_path=cf.nb_prefix_en_path, 
         output_data_path=cf.europarl_es_path, 
         output_nb_prefix_path=cf.nb_prefix_es_path,
-        MAX_LENGTH=20,
-        BATCH_SIZE = 64,
-        BUFFER_SIZE = 20000
+        MAX_LENGTH=cf.MAX_LENGTH,
+        BATCH_SIZE=cf.BATCH_SIZE,
+        BUFFER_SIZE=cf.BUFFER_SIZE
     )
 
     dataset = cd.get_dataset()
 
     transformer = Transformer(
-        vocab_size_enc=VOCAB_SIZE_EN,
-        vocab_size_dec=VOCAB_SIZE_ES,
-        d_model=D_MODEL,
-        nb_layers=NB_LAYERS,
-        FFN_units=FFN_UNITS,
-        nb_proj=NB_PROJ,
-        dropout_rate=DROPOUT_RATE
+        vocab_size_enc=cf.VOCAB_SIZE_EN,
+        vocab_size_dec=cf.VOCAB_SIZE_ES,
+        d_model=cf.D_MODEL,
+        nb_layers=cf.NB_LAYERS,
+        FFN_units=cf.FFN_UNITS,
+        nb_proj=cf.NB_PROJ,
+        dropout_rate=cf.DROPOUT_RATE
     )
--- app/config.py
+++ app/config.py
@@ -9,3 +9,12 @@ nb_prefix_es_path = "data/nonbreaking_prefix.es"
 OUTPUTS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../output'))
 INPUTS_FILE = os.path.join(OUTPUTS_DIR, 'inputs.csv')
 OUTPUTS_FILE = os.path.join(OUTPUTS_DIR, 'outputs.csv')
+
+MAX_LENGTH=20
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+D_MODEL = 128 # 512
+NB_LAYERS = 4 # 6
+FFN_UNITS = 512 # 2048
+NB_PROJ = 8 # 8
+DROPOUT_RATE = 0.1 # 0.1
